091553cd5cf80e641c1764c7cdcf4ce3311db615
Beata <beatach@hotmail.com>
2015-06-24 16:11:01 -0400

    making the app finally run, added missing dependencies

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,21 @@
 'use strict'; 
 var myApp = angular.module('myApp',[
 	'ngRoute',
-	'ngMaterial', 
-	'SummaryController'
+	'ngMaterial'
+//	'SummaryController'
 	]);
 
 myApp.config(['$routeProvider',function($routeProvider){
 	$routeProvider.
 	when('/summary', {
-		templateUrl: 'views/summary.html',
-		controller: 'SummaryController'
+		templateUrl: 'views/summary.html'
+	//	controller: 'SummaryController'
 	}).
 	otherwise({
 		redirectTo: '/summary'
 	});
 }])
-
-myApp.controller('SummaryController', function(){
-	console.log("SummaryController called"); 
-})
+//
+//myApp.controller('SummaryController', function(){
+//	console.log("SummaryController called"); 
+//})
