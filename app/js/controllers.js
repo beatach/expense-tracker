b2dee04623f277f6d841600e9c2fb608624f92dd
Beata <beatach@hotmail.com>
2015-06-26 09:00:08 -0400

    adding an add button with add icon

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,5 +3,12 @@
 var controllersModule = angular.module('ControllersModule', []);
 
 controllersModule.controller('ListController', ['$scope', function($scope){
+	$scope.expList = [
+		{"category": "food", "label":"Food", "total":457.23},
+		{"category": "clothes", "label":"Clothes", "total":171.99},
+		{"category": "gifts", "label":"Gifts", "total":45.99}
+	];
+
+
 	console.log("ListController called"); 
 }]);
