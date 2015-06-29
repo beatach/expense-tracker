521db5dd0284f10a3944e774ad0c859a5c0cb90d
Beata <beatach@hotmail.com>
2015-06-29 10:19:49 -0400

    Adding a controller for adding items

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,31 @@
 
 var controllersModule = angular.module('ControllersModule', []);
 
-controllersModule.controller('ListController', ['$scope', function($scope){
+controllersModule.controller('ListController', ['$scope', '$mdDialog', function($scope, $mdDialog){
 	$scope.expList = [
 		{"category": "food", "label":"Food", "total":457.23},
 		{"category": "clothes", "label":"Clothes", "total":171.99},
 		{"category": "gifts", "label":"Gifts", "total":45.99}
 	];
 
-
-	console.log("ListController called"); 
+	$scope.addItem = function(bucket, event){
+		$mdDialog.show({
+			controller: AddController,
+			templateUrl: 'addItem.html',
+			parent: angular.element(document.body),
+			targetEvent: event
+		});
+	};
 }]);
+
+function AddController($scope, $mdDialog){
+	$scope.hide = function(){
+		$mdDialog.hide();
+	};
+	$scope.cancel = function(){
+		$mdDialog.cancel();
+	};
+	$scope.answer = function(){
+		$mdDialog.hide(answer); 
+	};
+}
